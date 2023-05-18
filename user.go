03081af5263cfc335e84fdedecd23c82a94f04fa
Nimit Bhandari <nimitbhandari@outlook.com>
2023-05-18 11:29:26 +0530

    Add the user registration method to the app

--- user.go
+++ user.go
@@ -1,9 +1,20 @@
 package twitter
 
-import "time"
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var (
+	ErrUserNameTaken = errors.New("username is already taken")
+	ErrEmailTaken    = errors.New("email is already taken")
+)
 
 type UserRepo interface {
-	
+	Create(ctx context.Context, user User) (User, error)
+	GetByUsername(ctx context.Context, username string) (User, error)
+	GetByEmail(ctx context.Context, email string) (User, error)
 }
 
 type User struct {
